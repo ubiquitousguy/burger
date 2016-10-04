424edb2e28172a5f7665f93186e6dc208525f8d3
Guy <guypham@hotmail.com>
2016-10-04 14:48:55 -0700

    worked on orm.js page

--- config/orm.js
+++ config/orm.js
@@ -9,6 +9,18 @@ var orm = {
   },
 
   devourBurger: function(burgerid, cb) {
-    connection.query('UPDATE burgers SET devoured = ? WHERE ID = ?')
-  }
-}
+    connection.query('UPDATE burgers SET devoured = ? WHERE ID = ?', [1, burgerid], function(err, result) {
+            if (err) throw err;
+            cb();
+        });
+  },
+
+  addBurger: function(addBurger, cb) {
+        connection.query('INSERT INTO burgers SET ?' , {burger_name : addBurger}, function(err, result) {
+        if (err) throw err;
+        cb();
+        });
+    }
+};
+
+module.exports = orm;
