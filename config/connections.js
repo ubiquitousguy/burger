5e8c95e3d2d4a4556645b18dfead1aa5e606cf95
Guy <guypham@hotmail.com>
2016-10-06 14:27:34 -0700

    commented out the root section

--- config/connections.js
+++ config/connections.js
@@ -5,7 +5,7 @@ if(process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else{
     connection = mysql.createConnection({
-        root: 3306,
+        // root: 3306,
         host: 'localhost',
         user: 'root',
         password: 'guy',
